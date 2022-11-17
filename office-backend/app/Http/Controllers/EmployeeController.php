54662175857064f5dfbd908fef197bd438ac30ba
PedroLopes7K <pedrohenri3601@gmail.com>
2022-11-17 20:01:04 -0300

    finish employee controller

---
+++ office-backend/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $employees = new Employee([
+            'name' => $request->input('name'),
+            'address' => $request->input('address'),
+            'mobile' => $request->input('mobile'),
+
+        ]);
+        $employees->save();
+        return response()->json('Employee created!');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $employees = Employee::find($id);
+        $employees->update($request->all());
+        return response()->json('Employee updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $employees = Employee::find($id);
+        $employees->delete();
+        return response()->json(' deleted!');
+    }
+}
